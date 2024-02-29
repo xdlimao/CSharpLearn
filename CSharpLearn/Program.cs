11df5113aff70f4ea5ef5bd0779ec3fcb2c80179
Daniel <danieloliveiradesouza09@outlook.com>
2024-02-29 15:36:59 -0300

    editor de texto

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            string? texto = "";
+            Console.WriteLine("Teste de ESC");
+            while (true) {
+                texto += Console.ReadLine();
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    Console.WriteLine("Aapertou ESC");
+            };
         }
     }
 }
---
+++ EditorDeTexto/Program.cs
@@ -0,0 +1,79 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace EditorDeTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Menu();
+        }
+        void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+        void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+        void Editar ()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("---------------------------------------");
+
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+        }
+        void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            //Este using é super útil para controlar instância de objetos, no caso, ele ira instância um objeto e fechar quando acabar
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
