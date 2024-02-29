1aad1aafa96b29fca9b73212ab55560f17561c75
Daniel <danieloliveiradesouza09@outlook.com>
2024-02-29 17:58:38 -0300

    fim

--- EditorHtml/Program.cs
+++ EditorHtml/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace CSharpLearn
+﻿namespace EditorHtml
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
