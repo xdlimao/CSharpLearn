99fe59792dd038f76fcf94d121657c11dc3fe1ac
Daniel <danieloliveiradesouza09@outlook.com>
2024-03-05 19:14:40 -0300

    Enviando exemplo de Event

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace CSharpLearn
+﻿using System;
+
+namespace CSharpLearn
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            OtherProgram op = new OtherProgram();
+            var room = new Room(3);
+            room.RoomSoldOutEvent += OnRoomSoldOut;
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+        }
+        public static void OnRoomSoldOut(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Sala lotada");
+        }
+    }
+
+    public class Room
+    {
+        public Room (int seats)
+        {
+            Seats = seats;
+        }
+
+        private int seatsInUse = 0;
+        public int Seats { get; set; }
+
+        public void ReserveSeat()
+        {
+            seatsInUse++;
+            if (seatsInUse >= Seats)
+            {
+                OnRoomSoldOut(EventArgs.Empty);
+            } else
+            {
+                Console.WriteLine("Assento em uso");
+            }
+        }
+
+        public event EventHandler RoomSoldOutEvent;
+
+        protected virtual void OnRoomSoldOut(EventArgs e)
+        {
+            EventHandler handler = RoomSoldOutEvent;
+            handler?.Invoke(this, e);
         }
     }
 }
---
+++ CSharpLearn/StaticDream.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearn
+{
+    static class StaticDream
+    {
+        public static string xdders = "oii";
+        public static void Metodooo()
+        {
+            Console.WriteLine("Holesheeter");
+        }
+    }
+}
