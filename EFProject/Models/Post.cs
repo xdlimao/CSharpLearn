76738aaa7590e0aadf55823797bf8f2eb40152e8
Daniel <danieloliveiradesouza09@outlook.com>
2024-03-13 21:17:18 -0300

    xd2

--- EFProject/Models/Post.cs
+++ EFProject/Models/Post.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFProject.Models
 {
+    [Table("[Post]")]
     internal class Post
     {
+        [Key]
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public int AuthorId { get; set; }
--- EFProject/Models/Role.cs
+++ EFProject/Models/Role.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFProject.Models
 {
+    [Table("[Role]")]
     internal class Role
     {
         public int Id { get; set; }
